82a201f712dc933ba41a389d218adb26d3fd21fb
9qeklajc <9qeklajc>
2025-08-31 14:29:23 +0200

    clean up

--- src/nostr_mcp/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod client;
-pub mod encryption;
-pub mod memory_manager;
-pub mod server;
-pub mod types;
-
-pub use server::NostrMemoryServer;
-pub use types::*;
--- src/searxng_mcp/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod client;
-pub mod server;
-pub mod types;
-
-pub use server::SearXNGServer;
-pub use types::*;
